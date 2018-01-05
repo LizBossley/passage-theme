5a9688ee8fccfca17fb87f4505488221073d4658
lizbossley <liz.bossley@gmail.com>
2018-01-04 19:58:02 -0500

    continue progress

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'passage_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'passage' ),
+			'menu-top-right' => esc_html__( 'Header Top Right', 'passage' ),
 		) );
 
 		/*
--- header.php
+++ header.php
@@ -22,35 +22,50 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'passage' ); ?></a>
+	<a class="skip-link screen-reader-text show-for-sr" href="#content"><?php esc_html_e( 'Skip to content', 'passage' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'passage' ); ?></button>
+		<div class="header--top">	
 			<?php
 				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
+					'theme_location' => 'menu-top-right',
+					'menu_id'        => 'social-menu',
 				) );
 			?>
-		</nav><!-- #site-navigation -->
+		</div>
+		<div class="row expanded">
+			<div class="small-12 columns">
+				<div class="site-branding">
+					<?php
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+					endif;
+
+					$description = get_bloginfo( 'description', 'display' );
+					if ( $description || is_customize_preview() ) : ?>
+						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+					<?php
+					endif; ?>
+				</div><!-- .site-branding -->
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-12 columns main-navigation--wrapper">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle show-for-sr" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'passage' ); ?></button>
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						) );
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
